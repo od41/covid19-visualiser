bfeeab7b9d9066d262f7491817e0c7b7e23bbdfc
Odafe Aror <oaror@ipnxnigeria.net>
2020-04-04 04:44:33 +0100

    nigeria map rendered

--- src/components/MapChart.js
+++ src/components/MapChart.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {csv, scaleLinear} from 'd3';
+import {csv, scaleLinear, geoPath,  geoAlbersUsa} from 'd3';
 import {
     ComposableMap,
     Geographies,
@@ -7,9 +7,13 @@ import {
     Sphere,
     Graticule
 } from 'react-simple-maps';
+import nigeria from '../nigeria.json';
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+//   const naijaUrl = "https://github.com/deldersveld/topojson/blob/master/countries/nigeria/nigeria-states.json"
+// const naijaUrl =  fetch('/nigeria-states.json');
 
 
 const colorScale = scaleLinear()
@@ -23,33 +27,36 @@ const MapChart = () => {
         csv(`/vulnerability.csv`).then(data => {
             setData(data);
           });
+
     }, [])
 
     return (
         <ComposableMap
-          projectionConfig={{
-            rotate: [-10, 0, 0],
-            scale: 147
-          }}
+        //   projection={geoAlbersUsa()}
+          projectionConfig={{  
+              parallels: [0.8, 1],
+              rotate: [-7, -10, 0],
+              scale: 2500
+            }}
         >
-          <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
-          <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-          {data.length > 0 && (
-            <Geographies geography={geoUrl}>
+          {/* <Sphere stroke="#E4E5E6" strokeWidth={5} /> */}
+          {/* <Graticule stroke="#E4E5E6" strokeWidth={0.5} /> */}
+          
+            <Geographies geography={nigeria}>
               {({ geographies }) =>
                 geographies.map(geo => {
-                  const d = data.find(s => s.ISO3 === geo.properties.ISO_A3);
+                //   const d = data.find(s => s.ISO3 === geo.properties.ISO_A3);
                   return (
                     <Geography
                       key={geo.rsmKey}
                       geography={geo}
-                      fill={d ? colorScale(d["2017"]) : "#F5F4F6"}
+                      fill={"#F5F4F6"}
+                      stroke={"#000000"}
                     />
                   );
                 })
               }
             </Geographies>
-          )}
         </ComposableMap>
       );
 }
